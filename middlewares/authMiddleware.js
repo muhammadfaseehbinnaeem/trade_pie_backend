6edf2b3ade33df2fdb89bdce546d5ecb3067726c
muhammadfaseehbinnaeem <muhammadfaseehbinnaeem6@gmail.com>
2023-08-05 01:37:19 +0500

    log3

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = asyncHandler(async (req, res, next) => {
     // Read the JWT from the cookie
     token = req.cookies.jwt;
 
-    console.log('token: ' +token);
+    console.log('token: ' +req.cookie);
     if 
     (token) {
         try {
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -13,7 +13,7 @@ const generateToken = (res, userId) => {
         sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000    // 30 days
     });
-    console.log('setToken: '+ token);
+    
     return token;
 };
 
