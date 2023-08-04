e43170738126b40e1f37974d6d9b9fb45f703aea
muhammadfaseehbinnaeem <muhammadfaseehbinnaeem6@gmail.com>
2023-08-05 01:26:49 +0500

    log correction

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,15 +11,15 @@ const protect = asyncHandler(async (req, res, next) => {
     // Read the JWT from the cookie
     token = req.cookies.jwt;
 
-    log('token: ' +token);
+    console.log('token: ' +token);
     if 
     (token) {
         try {
             const decode = jwt.verify(token, process.env.JWT_KEY);
-            log('decode: ' + decode);
+            console.log('decode: ' + decode);
             
             req.user = await User.findById(decode.userId).select('-password');
-            log('user: ' +req.user);
+            console.log('user: ' +req.user);
             
             if (!req.user) {
                 req.user = await Admin.findById(decode.userId).select('-password');
